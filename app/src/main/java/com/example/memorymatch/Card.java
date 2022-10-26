6a8bb334ac33ffc676c6bdceb11cbbb502545403
omPatel1718 <67968273+omPatel1718@users.noreply.github.com>
2022-10-26 12:48:37 -0500

    better constructor

--- app/src/main/java/com/example/memorymatch/Card.java
+++ app/src/main/java/com/example/memorymatch/Card.java
@@ -1,13 +1,27 @@
 package com.example.memorymatch;
 
 public class Card {
-    int value;
+    int points, turns, time;
     boolean flipped, matched;
 
-    public Card(int v){
+    public Card(int value, String type){
         flipped = false;
         matched = false;
-        value = v;
+
+        if(type.equals("points")){
+            points = value;
+            turns = 0;
+            time = 0;
+        }else if(type.equals("turns")){
+            points = 0;
+            turns = value;
+            time = 0;
+        }
+        else{
+            points = value;
+            turns = 0;
+            time = 0;
+        }
     }
 
     public void clicked(){
@@ -17,11 +31,27 @@ public class Card {
     }
 
     public int getPoints() {
-        return value;
+        return points;
     }
 
-    public void setPoints(int value) {
-        this.value = value;
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getTurns() {
+        return turns;
+    }
+
+    public void setTurns(int turns) {
+        this.turns = turns;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
     }
 
     public boolean isFlipped() {
