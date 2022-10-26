925f7001868a762ef9c104fe816423fd67db9df7
omPatel1718 <67968273+omPatel1718@users.noreply.github.com>
2022-10-26 12:40:47 -0500

    basic card stuff

--- app/src/main/java/com/example/memorymatch/Card.java
+++ app/src/main/java/com/example/memorymatch/Card.java
@@ -1,14 +1,42 @@
 package com.example.memorymatch;
 
 public class Card {
-    int points, time, flips;
+    int value;
     boolean flipped, matched;
 
-    public Card(int p, int t, int f){
-        points = p;
-        time = t;
-        flips = f;
+    public Card(int v){
         flipped = false;
         matched = false;
+        value = v;
+    }
+
+    public void clicked(){
+        if(!flipped){
+            flipped = true;
+        }
+    }
+
+    public int getPoints() {
+        return value;
+    }
+
+    public void setPoints(int value) {
+        this.value = value;
+    }
+
+    public boolean isFlipped() {
+        return flipped;
+    }
+
+    public void setFlipped(boolean flipped) {
+        this.flipped = flipped;
+    }
+
+    public boolean isMatched() {
+        return matched;
+    }
+
+    public void setMatched(boolean matched) {
+        this.matched = matched;
     }
 }
