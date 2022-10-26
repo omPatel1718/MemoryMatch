5fe98dbabae84c6abb037026f675fd23fd0578c9
omPatel1718 <67968273+omPatel1718@users.noreply.github.com>
2022-10-26 12:24:23 -0500

    activities made

--- app/src/main/java/com/example/memorymatch/GameOverActivity.java
+++ app/src/main/java/com/example/memorymatch/GameOverActivity.java
@@ -1,13 +1,14 @@
 package com.example.memorymatch;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class GameOverActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game_over);
     }
 }
