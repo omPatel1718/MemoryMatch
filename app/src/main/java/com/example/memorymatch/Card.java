56f9d0ec3f5ec65e8a64244e0eb1caad29cd1437
omPatel1718 <67968273+omPatel1718@users.noreply.github.com>
2022-10-29 13:24:18 -0500

    added lives, and updated comments

--- app/src/main/java/com/example/memorymatch/Card.java
+++ app/src/main/java/com/example/memorymatch/Card.java
@@ -1,25 +1,33 @@
 package com.example.memorymatch;
 
 public class Card {
-    int points, turns, time;
+    int points, turns, time, lives;
     boolean flipped, matched;
 
     public Card(int value, int type){
         flipped = false;
         matched = false;
 
-        if(type == 1){                        //points
+        if(type == 1){                         //points - 1
             points = value;
-            turns = 0;
             time = 0;
-        }else if(type == 2){                  //turns
+            turns = 0;
+            lives = 0;
+        }else if(type == 2){                   //time - 2
             points = 0;
-            turns = value;
-            time = 0;
-        }else if(type == 3){                   //time
-            points = value;
+            time = value;
             turns = 0;
+            lives = 0;
+        }else if(type == 3){                   //turns - 3  (Zen only)
+            points = 0;
+            turns = 0;
+            time = value;
+            lives = 0;
+        }else if(type==0){                     //lives - 0  (Mastery only)
+            points = 0;
             time = 0;
+            turns = 0;
+            lives = value;
         }
     }
 
