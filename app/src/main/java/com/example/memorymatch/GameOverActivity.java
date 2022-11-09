340858dc32f17c1020edf6955cd440865a4a6a82
omPatel1718 <67968273+omPatel1718@users.noreply.github.com>
2022-11-09 12:51:04 -0600

    made score and game mode displays on gameOver functional

--- app/src/main/java/com/example/memorymatch/GameOverActivity.java
+++ app/src/main/java/com/example/memorymatch/GameOverActivity.java
@@ -5,15 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class GameOverActivity extends AppCompatActivity {
+
+    String gameMode;
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
 
+        Intent intent = getIntent();
+        gameMode = intent.getStringExtra("MODE");
+        score = intent.getIntExtra("SCORE", 0);
 
+        TextView scoreText = findViewById(R.id.score);
+        scoreText.setText("Score: " + score + " points");
+        TextView modeText = findViewById(R.id.gameMode);
+        modeText.setText(gameMode);
     }
 
     public void quitToTitle(View view) {
