820f6141c2e851fc7a130ddfda002b499af2e855
omPatel1718 <67968273+omPatel1718@users.noreply.github.com>
2022-11-21 12:34:25 -0600

    better menu is functional

--- app/src/main/java/com/example/memorymatch/InfoActivity.java
+++ app/src/main/java/com/example/memorymatch/InfoActivity.java
@@ -11,7 +11,7 @@ public class InfoActivity extends AppCompatActivity {
 
     String origin = "";
     int page = 1;
-    TextView infoText;
+    TextView infoText, pageNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +23,36 @@ public class InfoActivity extends AppCompatActivity {
         origin = intent.getStringExtra("PREVSCREEN");
 
         infoText = findViewById(R.id.instructions);
+        pageNum = findViewById(R.id.pageNumber);
+    }
+
+    public void pageRight(View v){
+        page++;
+    }
+
+    public void pageLeft(View v){
+        page--;
     }
 
     public void pageCheck(){
+        pageNum.setText(""+page);
         if(page==1){
             infoText.setText("Welcome to Memory Match!\nThis game is designed to test your memory.\nWhen you start the game, you'll get 18 cards to match" +
                     " and matching a pair will net you rewards!");
         }else if(page==2){
-            infoText.setText("Classic: Cards get you points or more time. Try to make as many ");
+            infoText.setText("Classic: A Good Beginning\nCards get you points or more time.\nTry to make as many matches as you can with the time you've got!");
+        }else if(page==3){
+            infoText.setText("Zen: Strategy is Key\nInstead of a timer, you have turns.\nEach time you flip 2 cards, you lose a turn whether or not you make a match." +
+                    "\nTake as much time as you need to get a good score!\nCards get you points or more turns.");
+        }else if(page==4){
+            infoText.setText("Blitz: GO GO GO\nIn this fast paced game mode, you start with less time and time increases are lower, but time cards are more common." +
+                    "\nStay focused under pressure and do your best.\nCards give points or more time.");
+        }else if(page==4){
+            infoText.setText("Mastery: The Ultimate Challenge\nIn the final test of skill, you have both a ticking timer, but also lives." +
+                    "\nEach time you make a wrong match you lose a life.\nCards give points, more lives, and more time.\nGood Luck.");
+        }else if(page==5){
+            infoText.setText("What does difficulty do?:\nIt changes starting lives, turns, and time. " +
+                    "\nIt also shortens the time the cards are revealed to you at the beginning");
         }
     }
 
